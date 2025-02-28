b014aee9c9e94ccdf591c8822ae993ecd673a0a5
Oscarx21 <oscarcoradi95@hotmail.com>
2025-02-28 19:52:38 -0300

    Primeira branch

--- Aula02/Hello.java
+++ Aula02/Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
     public static void main(String[] args) {
+        World();
+    }
+
+    public static void World(){
         System.out.println("Hello World!");
     }
+
+    
 }
